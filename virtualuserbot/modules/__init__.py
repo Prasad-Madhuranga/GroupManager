275891701c424c7ff1d5a13c1917e7617566cbb8
SLdevilX <52373359+SLdevilX@users.noreply.github.com>
2021-02-05 08:17:55 +0530

    Update __init__.py

--- virtualuserbot/modules/__init__.py
+++ virtualuserbot/modules/__init__.py
@@ -52,7 +52,7 @@ else:
     dbstats = "Not Fine ❌"
 
 inlinestats = (
-    f"✘ SHOWING VirtualUserbot STATS ✘\n"
+    f"✘ SHOWING Ai.Userbot STATS ✘\n"
     f"VERSION = {currentversion} \n"
     f"DATABASE = {dbstats} \n"
     f"SUDO = {amiusingsudo} \n"
